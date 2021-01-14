c6d7933eb07c11821132d09b1c49b93da23dab53
vincent <hienha@163.com>
2021-01-14 10:27:22 +0800

    recent installed softwares

---
+++ cmus/lib.pl
@@ -0,0 +1,124 @@
+/Users/changfeng/Music/Inspire Capo Productions.mp3
+/Users/changfeng/Music/Otilia+–+Prisionera+(Rino+Aqua+&+MD+DJ+Remix).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/080.周华健 - 刀剑如梦（高品质）.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/Inspire Capo Productions.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/Otilia+–+Prisionera+(Rino+Aqua+&+MD+DJ+Remix).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/Prisionera - Various Artists.wav
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/bullet.wav
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/running away.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/use_bomb.wav
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/you did it.m4a
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/勇气-棉子.m4a
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/红日_李克勤.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/鸳鸯情歌 - 东方健.m4a
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/黄土之恋 古银州浩浩.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/暴风一族.m4a
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/你走以后1.0 王恩信Est-二胖u.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/卡拉永远OK.m4a
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/伤心的我_邝美云.m4a
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/相思的泪水 - 红蔷薇.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/妈妈你别老 余志富.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/梦里常相伴.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/雨季开始了.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/看见你的第一眼 - 望海高歌.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/不是所有人都会值得等.mp3
+/Users/changfeng/Music/彩虹 - 周杰伦.mp3
+/Users/changfeng/Music/华平哥音乐/鸳鸯情歌 - 东方健.m4a
+/Users/changfeng/Music/华平哥音乐/黄土之恋 古银州浩浩.mp3
+/Users/changfeng/Music/华平哥音乐/相思的泪水 - 红蔷薇.mp3
+/Users/changfeng/Music/华平哥音乐/妈妈你别老 余志富.mp3
+/Users/changfeng/Music/华平哥音乐/梦里常相伴.mp3
+/Users/changfeng/Music/华平哥音乐/看见你的第一眼 - 望海高歌.mp3
+/Users/changfeng/Music/华平哥音乐/不是所有人都会值得等.mp3
+/Users/changfeng/Music/从开始到现在 -- 张信哲.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/02 rong yao.mp3
+/Users/changfeng/Music/Otilia+-+Prisionera+(Remix+Rino+Aqua+&+MD+DJ).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/Otilia - Prisionera (Remix Rino Aqua & MD DJ).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/感 Sensuous/唉声叹气.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/01-Linux.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Acreix/Visions/1-01 Visions.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Ahrix/The Beginning/1-09 Courage.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Anthony Keyrouz_Romy Wave/Something Just Like This/1-01 Something Just Like This.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Approaching Nirvana/Blocking the Sky/1-03 You.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Asher Book/Here With You/01 Here With You.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Ava Max/Salt (The Remixes)/Salt.mp4
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Axero/Trip/1-01 Trip.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Canoo/Float EP/1-05 Lines and Light.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Capo Productions/Serenity/1-10 Inspire.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/CelDro/Aspire/1-01 Aspire.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/CelDro/Navy/1-01 Navy.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Classical Artists/越听越聪明3：莫扎特效应/第21号钢琴奏鸣曲.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/DAISHI DANCE_Cecile Corbel/WONDER Tourism/1-04 Take me hand.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/DEAMN/Save Me/1-01 Save Me.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/DEAMN/Sign/1-01 Sign.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Different Heaven/NCS_ Infinity/1-06 Nekozilla.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Dirk Reichardt/Kokowääh 2 (Original Motion Picture Soundtrack)/1-23 Weekend.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/DJ‹¡¿É/DJ‹¡¿É ¢ßÖ»ÊÖÕÚÌì/Ó¢ÎÄÂ‡àË.mp3
+/Users/changfeng/Music/酷狗音乐/英文慢嗨 - JD小可.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/DJ‹¡¿É/DJ‹¡¿É ¢ßÖ»ÊÖÕÚÌì/英文慢嗨 - DJ 小可.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/DJ‹¡¿É/DJ‹¡¿É ¢ßÖ»ÊÖÕÚÌì/英文慢嗨 - DJ 小可.m4a
+/Users/changfeng/Music/英文慢嗨 - DJ 小可.m4a.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Dripice/Escape/1-01 Escape.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Elektronomia/Sky High/1-01 Sky High.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Feint_Veela/Vagrant/1-01 Vagrant.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Fri_TomLeevis/Leevis/1-01 Leevis.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Frida Sundemo/Lit Up By Neon/02 Drawn to You.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Itro/Panda/1-01 Panda.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Janji/Horizon/1-01 Horizon.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/JJD/Aeon/Aeon.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/K-391/Sunshine/1-02 Summertime.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/kuwo/Unknown Album/kuwo.mp3
+/Users/changfeng/Music/杨千嬅 - 处处吻.mp3
+/Users/changfeng/Music/欢喜就好 (DJ A8版).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Laszlo/Fall To Light/1-01 Fall To Light.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Lensko/Heaven/1-01 Heaven.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Leona Lewis/Now That's What I Call Music! 29/14 Better In Time.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Mark Pride/Into The Deep Vol.02/1-12 River Flows In You (Original Mix).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Marshmello/Alone/1-01 Alone (Original Mix).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Martin Tungevaag/Wicked Wonderland/1-01 Wicked Wonderland.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Megan & Liz/Bad for Me/04 Closer To Me.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Mich/Skyland/1-01 Skyland.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Mike Candys_Maury/Miracles/1-01 Miracles (Radio Edit).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/MitiS/Ascension (MitiS Remix)/1-01 Ascension (Remix).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/MKJ/Time/1-01 Time.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Nick Glennie-Smith/Lion King 2 (Simba's Pride)/1-06 Love Will Find A Way (With Gene Miller).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Panda Eyes/Highscore/1-01 Highscore.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Pixie Paris/Es Rappelt Im Karton/Es Rappelt Im Karton.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Rootkit/Against The Sun/1-01 Against the Sun (Original Mix).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Sasha Sloan/Dancing With Your Ghost/01 Dancing With Your Ghost.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Sibel/The Diving Belle/I'm Sorry.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Space/Just Blue/1-01 Just Blue.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/The xx/xx/1-01 Intro.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Thomas Prime/Sky High/1-01 Sky High.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Tobu & Itro/Unknown Album/Sunburst.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Tobu_Itro/Sunburst/1-01 Sunburst.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/TomLeevis/Soaring/1-01 Soaring (Original Mix).mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Vicetone_Cozi Zuehlsdorff/Nevada/1-01 Nevada.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Virtual Riot/Energy Drink/1-01 Energy Drink.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/Wisp X/Vibrance/1-01 Vibrance.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/ËÕ_òÂÌ/‹¡ÓîÖæ/03 ‹¡Çé¸è.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/ÔÆ·Æ·Æ_ÀäÄ®/Unknown Album/ÕâÌõ∏Ö.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/ÖÜ»ª∏¡/Ê®_ó∏›Ó_≤çÖ÷ÌâÇú--¿áÎÒÌØπ–/ÄÑÄîµÄ≤–.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/ÖÜ∏ÜÂ◊/Ä§∏Ü◊ù/02 ¸øÎÒÒ»Ê◊¸èµÄÊ±πä.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/ÖÜ∏ÜÂ◊/Ä§∏Ü◊ù/11 µ≤Ïã.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/ÖÜ∏ÜÂ◊/ÖÜ∏ÜÂ◊µÄ__±ß_ÊÊÂ/08 ¸æ°◊ÆøÇò.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/梦然/少年/少年.mp4
+/Users/changfeng/Music/少年 梦然.mp4
+/Users/changfeng/Music/iTunes/iTunes Media/Music/王杰/万岁2001/不浪漫罪名.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/王贺/妈在家就在/妈在家就在.mp3
+/Users/changfeng/Music/华平哥音乐/妈在家就在 王贺.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/冯一航/爱上黄土坡/爱上黄土坡.mp3
+/Users/changfeng/Music/华平哥音乐/爱上黄土坡.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/周杰伦/说好不哭/说好不哭.mp3
+/Users/changfeng/Music/周杰伦-说好不哭.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/周笔畅/最美的期待/01 最美的期待.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/梁静茹/靜茹 & 情歌 別再为他流泪/02 没有如果.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/田馥甄/我的少女时代 电影原声带/01 小幸运.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/网络歌手/且试天下/故乡之恋___望海高歌.mp3
+/Users/changfeng/Music/华平哥音乐/故乡之恋.mp3
+/Users/changfeng/Music/飞向别人的.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/»ÆÆ·Ô_/º£ÀË/º£ÀË.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/ÑÀÑÀÀÖ&¸ñ◊ÓÙâ/Unknown Album/秋殇别恋 Original.mp3
+/Users/changfeng/Music/秋殇别恋 Original.mp3
+/Users/changfeng/Music/iTunes/iTunes Media/Music/ÕÅÉØº–/Unknown Album/Ï_»¶ÄãÃ»µÀÀí.mp3
+/Users/changfeng/Music/喜欢你没道理 -- 张韶涵.mp3
